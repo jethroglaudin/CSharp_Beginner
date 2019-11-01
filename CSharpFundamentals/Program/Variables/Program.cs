744f1ed1db5c2db188f4d02cf9b076f0cbbbd0d6
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-01 17:13:13 -0400

    CSharp Fundamentals directory added

---
+++ CSharpFundamentals/Program/Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Jethro";
+            var isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            const float Pi = 3.14f;
+           
+        }
+    }
+}
+
