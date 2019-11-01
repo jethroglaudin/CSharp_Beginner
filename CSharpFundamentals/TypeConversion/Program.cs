619c993bead47b41f7670a70b33b9aa171e0aae2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-01 18:24:25 -0400

    TypeConversion console project

---
+++ CSharpFundamentals/TypeConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //byte b = 1; // 1 bite
+            //int i = b; // 4 bytes;
+            //Console.WriteLine(i);
+
+            //int i = 1;
+            //// cast interger to byte
+            //byte b = (byte)i;
+            //Console.WriteLine(b);
+
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+        }
+    }
+}
