9d350ba02c475115914499ac7724215c603917f6
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-02 18:28:37 -0400

    updated Program.cs added an enum called ShippingMethod

--- CSharpFundamentals/Non-PrimitiveTypes/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Non-PrimitiveTypes/CSharpFundamentals/Program.cs
@@ -3,18 +3,38 @@ using System;
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var jethro = new Person();
-            jethro.FirstName = "Jethro";
-            jethro.LastName = "Glaudin";
-            jethro.Introduce();
+            //var jethro = new Person();
+            //jethro.FirstName = "Jethro";
+            //jethro.LastName = "Glaudin";
+            //jethro.Introduce();
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            //Calculator calculator = new Calculator();
+            //var result = calculator.Add(1, 2);
+            //Console.WriteLine(result);
+
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            // parse the string to different type.
+            // parse string into an Enum
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
         }
     }
 }
