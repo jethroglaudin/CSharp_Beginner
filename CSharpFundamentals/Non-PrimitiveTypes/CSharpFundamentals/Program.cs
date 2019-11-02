ed8dfa40ea0e9fcd832bef7cba8cdd1ea00eb684
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-01 21:26:44 -0400

    added Math folder to store Calculator class that adds two intergers

---
+++ CSharpFundamentals/Non-PrimitiveTypes/CSharpFundamentals/Program.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFundamentals.Math;
+using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jethro = new Person();
+            jethro.FirstName = "Jethro";
+            jethro.LastName = "Glaudin";
+            jethro.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
