0813be27fe0705546bbe66eae590a68151984485
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-16 21:23:41 -0500

    Created Iterations class working on simple for loops

---
+++ CSharpFundamentals/Loops&Conditions/Loops/Iterations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Conditionals_Loops
+{
+    public class Iterations
+    {
+
+        public void Loop(int num1)
+        {
+
+            for (var i = 0; i <= num1 ; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = num1; i >= 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
