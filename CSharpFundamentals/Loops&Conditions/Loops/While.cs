d87313599383d0bbf8eead9b3e8bf866667c2701
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-16 22:53:12 -0500

    Created While class working on while loops

---
+++ CSharpFundamentals/Loops&Conditions/Loops/While.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Conditionals_Loops
+{
+    public class While
+    {
+
+        public void Loop()
+        {
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                break;
+            }
+
+        }
+    }
+}
