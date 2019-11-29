6730cf9361ec5c9780d91ed094ec4c25f2f9b9fe
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-28 22:58:02 -0500

    working with arrays and array methods like IndexOf and Clear

--- CSharpFundamentals/Arrays_Lists/Program.cs
+++ CSharpFundamentals/Arrays_Lists/Program.cs
@@ -4,9 +4,16 @@ namespace Arrays_Lists
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new[] { 3, 5, 9, 2, 14, 6 };
+
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
         }
     }
 }
