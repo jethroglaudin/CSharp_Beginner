dff1669dc77e23b8166fd65fd2e3f78ff42a0bbc
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-11-28 23:11:07 -0500

    working with Array.copy to copy elements from one array to another.

--- CSharpFundamentals/Arrays_Lists/Program.cs
+++ CSharpFundamentals/Arrays_Lists/Program.cs
@@ -7,6 +7,7 @@ namespace Arrays_Lists
         static void Main(string[] args)
         {
             var numbers = new[] { 3, 5, 9, 2, 14, 6 };
+            Console.WriteLine("Array: " + numbers);
 
             // Length
             Console.WriteLine("Length: " + numbers.Length);
@@ -14,6 +15,22 @@ namespace Arrays_Lists
             // IndexOf
             var index = Array.IndexOf(numbers, 9);
             Console.WriteLine("Index of 9: " + index);
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            // Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of Copy");
+            foreach(var n in another)
+                Console.WriteLine(n);
         }
     }
 }
